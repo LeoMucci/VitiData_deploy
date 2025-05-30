80eac2fd8643a84bcf98a7fd21f5a7fcfc6e574f
LeoMucci <leocapramucci@gmail.com>
2025-05-30 18:40:01 -0300

    Swagger

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import List
 from app.scraper import (
     scrape_producao_pages,
     scrape_exportacao,
@@ -6,32 +7,33 @@ from app.scraper import (
     scrape_importacao,
     scrape_comercializacao_page
 )
-from app.routes import routes  
+from app.routes import routes
+from app.schemas.vinho import ComercioEntrada  # usado para todos os retornos
 
 app = FastAPI(title="VitiData API")
 
-app.include_router(routes.router)  
+app.include_router(routes.router)
 
-@app.get("/healthz")
+@app.get("/healthz", summary="Health Check", description="Verifica se a API está no ar.")
 def health():
     return {"status": "OK"}
 
-@app.get("/producao")
+@app.get("/producao", response_model=List[ComercioEntrada], summary="Dados de Produção", description="Retorna dados da aba 'Produção'.")
 def producao():
     return scrape_producao_pages()
 
-@app.get("/exportacao")
+@app.get("/exportacao", response_model=List[ComercioEntrada], summary="Dados de Exportação", description="Retorna dados da aba 'Exportação'.")
 def exportacao():
     return scrape_exportacao()
 
-@app.get("/processamento")
+@app.get("/processamento", response_model=List[ComercioEntrada], summary="Dados de Processamento", description="Retorna dados da aba 'Processamento'.")
 def processamento():
     return scrape_all_processamento()
 
-@app.get("/importacao")
+@app.get("/importacao", response_model=List[ComercioEntrada], summary="Dados de Importação", description="Retorna dados da aba 'Importação'.")
 def importacao():
     return scrape_importacao()
 
-@app.get("/comercializacao")
+@app.get("/comercializacao", response_model=List[ComercioEntrada], summary="Dados de Comercialização", description="Retorna dados da aba 'Comercialização'.")
 def comercializacao():
     return scrape_comercializacao_page()
